6161963532fbd3f91e8b711bf32decfb1e652b01
tatiana.anisimova <tznatkova90@gmail.com>
2024-12-11 13:54:42 +0300

    /11/12

--- src/main/java/TestSave.java
+++ src/main/java/TestSave.java
@@ -4,11 +4,23 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class TestSave {
 
     public static void main(String[] args) {
-       // add(new Employee("Петрова", "Валентина", "IT", 4500));
-        System.out.println(getById(2));
+        // add(new Employee("Петрова", "Валентина", "IT", 4500));
+        //  System.out.println(getById(2));
+        //  System.out.println(getAllEmp());
+        // add(new Employee("Катя","Иванова","Finance", 890));
+        // getAllByFilter("from Employee where surname = 'Anisimova'");
+      //  getAllByFilter("from Employee where surname = 'Anisimova' AND salary>1000");
+      //  updateById(2,100000);
+      //  updateWithCondition("update Employee set salary = 2000 where salary>2000");
+
+        //deleteById(3);
+
+        deleteByCondition("delete Employee where department='Finance' AND name='Катя'");
 
 
     }
@@ -36,12 +48,114 @@ public class TestSave {
         try {
             Session session = sf.getCurrentSession();
             session.beginTransaction();
-            Employee emp1 = session.get(Employee.class,id);
+            Employee emp1 = session.get(Employee.class, id);
             session.getTransaction().commit();
             return emp1;
         } finally {
             sf.close();
         }
     }
+
+    public static List<Employee> getAllEmp() {
+        SessionFactory sf = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = sf.getCurrentSession();
+            session.beginTransaction();
+            List<Employee> list = session.createQuery("from Employee").getResultList();
+            list.forEach(System.out::println);
+            session.getTransaction().commit();
+            return list;
+        } finally {
+            sf.close();
+        }
+    }
+
+    public static List<Employee> getAllByFilter(String condition) {
+        SessionFactory sf = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = sf.getCurrentSession();
+            session.beginTransaction();
+            List<Employee> list = session.createQuery(condition).getResultList();
+            list.forEach(System.out::println);
+            session.getTransaction().commit();
+            return list;
+        } finally {
+            sf.close();
+        }
+    }
+
+    public static void updateById (int id,int salary) {
+        SessionFactory sf = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = sf.getCurrentSession();
+            session.beginTransaction();
+            Employee emp = session.get(Employee.class,id);
+            emp.setSalary(salary);
+            session.getTransaction().commit();
+
+        } finally {
+            sf.close();
+        }
+    }
+    public static void updateWithCondition (String condition) {
+        SessionFactory sf = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = sf.getCurrentSession();
+            session.beginTransaction();
+           session.createQuery(condition).executeUpdate();
+            session.getTransaction().commit();
+
+        } finally {
+            sf.close();
+        }
+    }
+
+    public static void deleteById (int id) {
+        SessionFactory sf = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = sf.getCurrentSession();
+            session.beginTransaction();
+            Employee emp = session.get(Employee.class,id);
+            session.remove(emp);
+            session.getTransaction().commit();
+
+        } finally {
+            sf.close();
+        }
+    }
+
+    public static void deleteByCondition (String condition) {
+        SessionFactory sf = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = sf.getCurrentSession();
+            session.beginTransaction();
+
+            session.createQuery(condition).executeUpdate();
+            session.getTransaction().commit();
+
+        } finally {
+            sf.close();
+        }
+    }
+
+
 
 }
